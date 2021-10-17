707737b69c15644d3e68434346fbe6b9ec5f497f
Coder-fox-bd <ahrabbi121@gmail.com>
2021-10-17 13:39:19 +0600

    carousel problem solution 6

--- resources/views/components/why-us.blade.php
+++ resources/views/components/why-us.blade.php
@@ -31,3 +31,50 @@
         </defs>
     </svg>
 </div>
+
+
+
+
+
+
+
+
+<div class="w-80 lg:m-4 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-xl overflow-hidden bg-white my-12 mx-8">
+    <a href="#link">
+      <!-- Card Image -->
+      <img src="https://picsum.photos/id/342/2106/1404" alt="">
+      <!-- Card Content -->
+      <div class="p-4">
+        <h3 class="font-medium text-gray-600 text-lg my-2 uppercase truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id commodi illo quisquam eaque ex nostrum totam debitis accusamus iure ullam, alias eos sed consequuntur. Soluta dolorum natus ipsum iste esse.</h3>
+        <div class="flex items-center">
+          {{-- <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
+          <span class="text-xs ml-2">by Test User</span>
+        </div>
+        <div class="relative pt-1 mt-2">
+          <span><strong>$30000</strong> <span class="text-gray-400">raised out of $100000</span></span>
+          <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+            <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+          </div>
+        </div>
+        <div class="flex mt-10">
+          <div class="flex items-center">
+            <svg class="w-4 h-auto" id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m479.451 112.98-53.74-53.74c-7.811-7.811-20.474-7.811-28.284 0s-7.811 20.474 0 28.284l12.728 12.728-26.102 26.102c-33.978-28.346-75.292-45.458-119.362-49.441v-36.913h18c11.046 0 20-8.954 20-20s-8.954-20-20-20h-76c-11.046 0-20 8.954-20 20s8.954 20 20 20h18v36.913c-109.724 9.916-198 102.055-198 217.087 0 120.482 97.501 218 218 218 120.482 0 218-97.501 218-218 0-51.511-17.756-100.286-50.354-139.362l26.102-26.102 12.728 12.728c7.81 7.81 20.473 7.811 28.284 0 7.81-7.81 7.81-20.473 0-28.284zm-234.76 359.02c-98.149 0-178-79.851-178-178s79.851-178 178-178 178 79.851 178 178-79.85 178-178 178zm103.923-178c0 11.046-8.954 20-20 20h-83.922c-11.046 0-20-8.954-20-20v-117.675c0-11.046 8.954-20 20-20s20 8.954 20 20v97.675h63.922c11.046 0 20 8.954 20 20z"/></svg>
+            <span class="text-xs ml-2"><strong>17</strong></span>
+            <span class="text-xs text-gray-400 ml-1">Days left</span>
+          </div>
+          <div class="flex items-center ml-auto">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heart" class="w-4 h-auto text-red-600" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+            <span class="text-xs ml-2"><strong>24534</strong></span>
+            <span class="text-xs text-gray-400 ml-1">Supporters</span>
+          </div>
+        </div>
+        <div class="flex mt-2">
+          <a href="#"
+            class="flex items-center bg-transparent mr-auto hover:bg-blue-500 text-blue-500 hover:text-white rounded shadow hover:shadow-lg py-2 px-8 border border-blue-500 hover:border-transparent">
+            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-square" class="w-4 h-auto" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path></svg>
+            &nbsp;Share</a>
+          <a href="#" class="hover:bg-gray-700 rounded py-2 px-9 font-semibold hover:text-white bg-gray-400 text-gray-100">Donate</a>
+        </div>
+      </div>
+    </a>
+</div>
