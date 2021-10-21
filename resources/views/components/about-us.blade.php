a28ad3634dd96ff2420a3eeabf2b6c176fc08540
Coder-fox-bd <ahrabbi121@gmail.com>
2021-10-22 02:18:44 +0600

    Update about-us.blade.php

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,8 +1,6 @@
 <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
 <div class="relative overflow-hidden">
-    <div class="container mx-auto px-6 md:px-12 relative flex items-center py-24 xl:py-20">
-        <div class="grid justify-items-center grid-cols-1 lg:grid-cols-2 p-4 gap-4">
-            {!! $about->description !!}
-        </div>
+    <div class="container py-10 md:mx-auto px-8 md:px-auto">
+        {!! $about->description !!}
     </div>
 </div>
