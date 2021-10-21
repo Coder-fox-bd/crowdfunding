eaaedcde219a02b6bce24529c567c2c6ed6dad87
Coder-fox-bd <ahrabbi121@gmail.com>
2021-10-22 02:37:12 +0600

    ckeditor image align fixed

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -1,6 +1,6 @@
 <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
 <div class="relative overflow-hidden">
-    <div class="container py-10 md:mx-auto px-8 md:px-auto">
+    <div class="ck-content container py-10 md:mx-auto px-8 md:px-auto">
         {!! $about->description !!}
     </div>
 </div>
